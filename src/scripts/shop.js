95184790629a58030c2318ca037dbe228cc85493
CamiloGuevara19 <camilogr0419@gmail.com>
2022-05-15 22:26:12 -0500

    product verification

--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -6,6 +6,8 @@ const categoryFilter = document.getElementById("category");
 const priceFilter = document.getElementById("order");
 
 let products = [];
+let cart = getMycart();
+console.log(cart);
 
 async function loadProducts() {
     const firebaseProducts = await getProducts(db);
@@ -32,6 +34,10 @@ function renderProduct(item) {
 
     const coverImage = item.images ? item.images[0] : "https://latarta.com.co/wp-content/uploads/2018/06/default-placeholder-300x300.png"; 
 
+    const validateProduct = cart.some((productCart) => productCart.id === item.id);
+
+    const productButtonCart = validateProduct ? '<button class="products__btn" disabled>Added to cart</button>' : '<button class="products__btn">Add to Cart</button>'
+
     product.innerHTML = `
     <article class="products__box">
     <img src="${coverImage}" alt="" class="products__img">
@@ -40,12 +46,33 @@ function renderProduct(item) {
 
         <h4 class="products__title">${item.name}</h4>
         <h3 class="products__price">$${item.price}</h3>
-        <button class="products__cart">Add to Cart</button>
+        ${productButtonCart}
     </div>
     </article>
     `;
 
     productSection.appendChild(product);
+
+    const addProductButton  = product.querySelector(".products__btn");
+
+    addProductButton.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        addProductToCart();
+        cart.push(item);
+
+        //addProductButton.setAttribute("disabled", true);
+        addProductButton.innerText = "Product added!"
+    });
+}
+
+function addProductToCart(){
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function getMycart(){
+    const myCart = localStorage.getItem("cart");
+    return myCart ? JSON.parse(myCart) : [];
 }
 
 function filterBy() {
@@ -63,13 +90,14 @@ function filterBy() {
         filteredProduct = products;
     }
 
+    if (newOrder === "upward") {
+        filteredProduct = filteredProduct.sort((a, b) => a.price - b.price);
+    }
+
     if (newOrder === "downward") {
         filteredProduct = filteredProduct.sort((a ,b) => b.price - a.price);
     }
 
-    if (newOrder === "upward") {
-        filteredProduct = filteredProduct.sort((a, b) => a.price - b.price);
-    }
 
     productSection.innerHTML="";
     filteredProduct.forEach( product => {
