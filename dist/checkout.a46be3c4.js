32ef703984fed78e54ff4d4e4ca3204487a4abc2
Camilo Guevara Rengifo <1007619725@u.icesi.edu.co>
2022-05-16 08:16:16 -0500

    setting pages with firebase

---
+++ dist/checkout.a46be3c4.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("checkoutForm");e.addEventListener("submit",(async a=>{a.preventDefault(),console.log("finish");const l=e.name.value,t=e.cardnumber.value,n=e.csc.value,u=e.date.value,c=e.email.value;""!==l&&""!==t&&""!==n&&""!==u&&""!==c?alert("Thank you for your purchase"):alert("Please fill every input")}));
+//# sourceMappingURL=checkout.a46be3c4.js.map
